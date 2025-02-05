7b7eccdcc3fe38403585715a93db44fcbe8fa81f
Italo Pessoa <italo.pessoa@hotmail.com>
2025-02-04 23:43:51 -0300

    fix: add namespace

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -30,17 +30,17 @@ data "aws_eks_cluster" "techchallenge_cluster" {
 }
 
 
-provider "kubernetes" {
-  config_path    = "C:\\Users\\italo\\.kube\\config"
-  config_context = "minikube"
-}
-
 # provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.techchallenge_cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.techchallenge_cluster.certificate_authority[0].data)
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.techchallenge_cluster.name]
-#   }
+#   config_path    = "C:\\Users\\italo\\.kube\\config"
+#   config_context = "minikube"
 # }
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.techchallenge_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.techchallenge_cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.techchallenge_cluster.name]
+  }
+}
